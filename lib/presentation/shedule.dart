7ec1f49beaf27a247298423a734da16cbd63f494
m1rmen <emir.melgaziev@gmail.com>
2024-04-14 22:01:19 +0500

    Added datapicker

--- lib/presentation/shedule.dart
+++ lib/presentation/shedule.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+// https://pub.dev/packages/table_calendar
 
 class SchedulePage extends StatefulWidget {
   const SchedulePage({super.key});
@@ -12,16 +14,31 @@ class _SchedulePageState extends State<SchedulePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Расписание', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-        actions: <Widget>[
-          IconButton(onPressed: () { print("Нажата кнопка меню"); }, icon: Icon(Icons.menu), color: Colors.white,)
-        ],
+        leading: const Icon(Icons.menu, color: Colors.white,),
+        title: const Text('Расписание', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),),
         backgroundColor: const Color(0xff007bff),
       ),
-      body: const SingleChildScrollView(
-        child: const Column(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            const Text('123')
+        TableCalendar(
+          firstDay: DateTime.utc(2024, 1, 1),
+          lastDay: DateTime.utc(2025, 1, 1),
+          focusedDay: DateTime.now(),
+          calendarFormat: CalendarFormat.week, // сколько дней отображать (неделя, 2, месяц)
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false, // отключает кнопку выбора отображения (неделя, 2 недели, месяц)
+            rightChevronVisible: false, // отключает стрелку вправо для перемещения
+            leftChevronVisible: false, // отключает стрелку влево для перемещения
+            headerPadding: EdgeInsets.all(15), // добавляет отступы
+          ),
+          calendarStyle: CalendarStyle(
+            todayDecoration: BoxDecoration( // изменяет дефолтную форму текущего дня
+                color: const Color(0xff007bff),
+                shape: BoxShape.circle
+            ),
+          ),
+      ),
           ],
         ),
       ),
