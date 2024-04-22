d76a16838e1ff2ee08e14177b5119a9151caa1f9
melgaziev@hmtpk <stud6720979369@gr.hmtpk.ru>
2024-04-22 12:58:10 +0500

    Starting work of schedule container

--- lib/presentation/shedule.dart
+++ lib/presentation/shedule.dart
@@ -22,6 +22,51 @@ class _SchedulePageState extends State<SchedulePage> {
         child: Column(
           children: [
             TableCalendarWidget(), // lib/widgets/tablecalendar.dart
+            Padding(padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.015)),
+            //Text('123273627352352735662735273527352735527352735')
+            Container(
+              // color: Colors.red,
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 200,
+              child: Column(
+                children: [
+                  Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text('8:00'),
+                            Text('8:45')
+                          ],
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.05,
+                        ),
+                        Expanded(
+                          child: Text(
+                            'Производственная практика "Создание мобильных приложений на Flutter"',
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 4,
+                            softWrap: true,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.person, size: 16,),
+                      Text('Руководитель практики', style: TextStyle(
+                        fontWeight: FontWeight.w100,
+                        fontSize: 12
+                      ),),
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
