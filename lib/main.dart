34f32f247f2cc46f7b8d147b4d3ec0f5fc2462ef
melgaziev@hmtpk <stud6720979369@gr.hmtpk.ru>
2024-04-24 11:20:50 +0500

    remove debugBanner, rename widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hmtpk/presentation/shedule.dart';
+import 'package:hmtpk/presentation/shedulePage.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hmtpk/widgets/testshedule.dart';
+import 'package:hmtpk/widgets/lessonwidget.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'hmtpk',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
