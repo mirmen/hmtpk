aa27a62bf428e9b100310065f4711d6b3e412883
melgaziev@hmtpk <stud6720979369@gr.hmtpk.ru>
2024-04-27 11:28:18 +0500

    Optimization (const styles)

--- lib/widgets/tablecalendar.dart
+++ lib/widgets/tablecalendar.dart
@@ -18,15 +18,15 @@ class _TableCalendarWidgetState extends State<TableCalendarWidget> {
       lastDay: DateTime.utc(2025,1,1),
       focusedDay: DateTime.now(),
       calendarFormat: CalendarFormat.week, // сколько дней отображать (неделя, 2, месяц)
-      headerStyle: HeaderStyle(
+      headerStyle: const HeaderStyle(
         formatButtonVisible: false, // отключает кнопку выбора отображения (неделя, 2 недели, месяц)
         rightChevronVisible: false, // отключает стрелку вправо для перемещения
         leftChevronVisible: false, // отключает стрелку влево для перемещения
         headerPadding: EdgeInsets.all(15), // добавляет отступы
       ),
-      calendarStyle: CalendarStyle(
+      calendarStyle: const CalendarStyle(
         todayDecoration: BoxDecoration( // изменяет дефолтную форму текущего дня
-            color: const Color(0xff007bff),
+            color: Color(0xff007bff),
             shape: BoxShape.circle
         ),
       ),
