e00c315d417f3192065096dbd9193ca95ca1c2ef
melgaziev@hmtpk <stud6720979369@gr.hmtpk.ru>
2024-04-27 11:27:38 +0500

    rename import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hmtpk/presentation/shedulePage.dart';
+import 'package:hmtpk/presentation/shedulepage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
