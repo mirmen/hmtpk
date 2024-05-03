133c0721bf564da5b627d67fe4159eb607712aaf
melgaziev@hmtpk <stud6720979369@gr.hmtpk.ru>
2024-05-03 14:28:15 +0500

    del unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hmtpk/internal/application.dart';
 
-import 'package:hmtpk/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
