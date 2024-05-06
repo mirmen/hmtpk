77025894bc7227215e4ec9be6bb743cccd4068d4
melgaziev@hmtpk <stud6720979369@gr.hmtpk.ru>
2024-05-06 13:57:51 +0500

    ThemeProvider

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,26 +1,42 @@
+// internal/application.dart
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:hmtpk/internal/theme.dart';
 import 'package:hmtpk/presentation/screens/information_screen.dart';
 import 'package:hmtpk/presentation/screens/shedule_screen.dart';
+import 'package:provider/provider.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+}
 
 class Application extends StatelessWidget {
   const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'hmtpk',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-        fontFamily: GoogleFonts.montserrat().fontFamily,
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'hmtpk',
+            theme: themeProvider.isDarkMode ? darkTheme : lightTheme,
+            initialRoute: '/shedule',
+            routes: {
+              '/shedule': (context) => const SheduleScreen(),
+              '/information': (context) => const InformationScreen()
+            },
+          );
+        },
       ),
-      initialRoute: '/shedule',
-      routes: {
-        '/shedule': (context) => const SheduleScreen(),
-        '/information': (context) => const InformationScreen()
-      },
     );
   }
 }
